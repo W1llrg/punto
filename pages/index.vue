9c3e15d4ef00ad3cc5f3afd3396c1e3f1c4af713
Willrg <will.ragueneau@gmail.com>
2023-11-09 20:10:46 +0100

    🐛 FIX: just bin taking out the trash

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <h1 class="main-title"> Bienvenue sur le jeu du Punto</h1>
+        <h1 class="main-title" style="text-align: center;">Puntooooooo</h1>
     </div>
 </template>
