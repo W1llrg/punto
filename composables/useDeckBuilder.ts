8514f3527f4ec7fee08df99455c02c21a4225ee8
Willrg <will.ragueneau@gmail.com>
2023-11-09 22:11:37 +0100

    📦 NEW: Deck class

---
+++ composables/useDeckBuilder.ts
@@ -0,0 +1,14 @@
+
+export class Deck {
+  cards: Card[];
+  deckSize: number;
+
+  constructor(cards: Card[], deckSize: number) {
+    this.cards = cards;
+    this.deckSize = deckSize;
+  }
+}
+
+class Card {
+
+}
