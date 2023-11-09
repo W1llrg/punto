fffe534fc2bc83eeee1c871f5e8b4df79b57f577
Willrg <will.ragueneau@gmail.com>
2023-11-09 20:27:02 +0100

    📦 NEW: grid created

---
+++ components/Grid.vue
@@ -0,0 +1,51 @@
+
+<template>
+  <div class="grid">
+    <div v-for="(row, rowIndex) in grid" :key="rowIndex" class="row">
+      <div
+        v-for="(cell, cellIndex) in row"
+        :key="cellIndex"
+        class="cell"
+        :class="{ active: cell }"
+        @click="toggleCell(rowIndex, cellIndex)"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      grid: Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => false))
+    }
+  },
+  methods: {
+    toggleCell(rowIndex, cellIndex) {
+      this.grid[rowIndex][cellIndex] = !this.grid[rowIndex][cellIndex]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.grid {
+  display: flex;
+  flex-wrap: wrap;
+  width: 300px;
+}
+
+.row {
+  display: flex;
+}
+
+.cell {
+  width: 50px;
+  height: 50px;
+  border: 1px solid black;
+}
+
+.active {
+  background-color: green;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        <h1 class="main-title" style="text-align: center;">Puntooooooo</h1>
+    <h1 class="main-title" style="text-align: center;">Puntooooooo</h1>
+    <div clash="grid">
+        <Grid />
     </div>
 </template>
+
+<style scoped>
+    .grid {
+        display: flex;
+        flex-wrap: wrap;
+        align-content: center;
+    }
+</style>
