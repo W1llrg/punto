52f134a6d1821e474e2ea78810ab77cc9e116bfc
Willrg <will.ragueneau@gmail.com>
2023-12-03 18:20:27 +0100

    🐛 FIX: sqlite move insertion not adding IDs

--- databases/sqlite/table_creation_script.sql
+++ databases/sqlite/table_creation_script.sql
@@ -1,17 +1,19 @@
 CREATE TABLE
     Player (
-        name VARCHAR(150) NOT NULL,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(150) NOT NULL
     );
 
 CREATE TABLE
     Game (
-        datePlayed DATETIME NOT NULL,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        datePlayed DATETIME NOT NULL
     );
 
 CREATE TABLE
     PlayerGame (
-        player_id INT,
-        game_id INT,
+        player_id INTEGER,
+        game_id INTEGER,
         PRIMARY KEY (player_id, game_id),
         FOREIGN KEY (player_id) REFERENCES Player(id),
         FOREIGN KEY (game_id) REFERENCES Game(id)
@@ -19,18 +21,19 @@ CREATE TABLE
 
 CREATE TABLE
     Moves (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         name VARCHAR(150) NOT NULL,
-        value INT NOT NULL,
-        played_by INT,
-        game_id INT,
+        value INTEGER NOT NULL,
+        played_by INTEGER,
+        game_id INTEGER,
         FOREIGN KEY (played_by) REFERENCES Player(id),
         FOREIGN KEY (game_id) REFERENCES Game(id)
     );
 
 CREATE TABLE
     Winner (
-        game_id INT,
-        player_id INT,
+        game_id INTEGER,
+        player_id INTEGER,
         PRIMARY KEY (player_id, game_id),
         FOREIGN KEY (game_id) REFERENCES Game(id),
         FOREIGN KEY (player_id) REFERENCES Player(id)
