29cf2b5e6231cd79f43b05aaa253fc9171dcce1b
Willrg <will.ragueneau@gmail.com>
2023-12-03 18:32:00 +0100

    📦 NEW: set winner route

--- components/Grid.vue
+++ components/Grid.vue
@@ -213,6 +213,9 @@ export default {
             if (hMax.includes(5) || vMax.includes(5) || dMax.includes(5)) {
                 this.gameWonBy = player.getName();
                 console.log(`game won by ${this.gameWonBy}!`);
+
+                // database game end
+                axios.post(`http://localhost:3001/sqlite/set-winner/${this.gameWonBy}`);
             }
         },
 
